73e0ba5dd110466908b867b26afe21fc0615420f
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-04-10 09:17:49 +0530

    created a new app for searching movie , added obdb api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Search from "./components/Search";
+
+const api = {
+  key: `${process.env.REACT_APP_MOVIE_API_KEY}`,
+  base: "http://www.omdbapi.com/?apikey",
+};
 
 function App() {
+  const apiurl = `${api.base}=${api.key}`;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movie Database</h1>
       </header>
+      <main>
+        <Search />
+      </main>
     </div>
   );
 }
